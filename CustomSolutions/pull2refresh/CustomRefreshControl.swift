3b791843461d39b6ca0dec6f2af4ad82caa797cd
Slava Zubrin <slava@Slavas-Mac-mini.local>
2017-09-19 13:12:09 +0300

    WIP on custom refresh control

---
+++ CustomSolutions/pull2refresh/CustomRefreshControl.swift
@@ -0,0 +1,54 @@
+//
+//  CustomRefreshControl.swift
+//  CustomSolutions
+//
+//  Created by Slava Zubrin on 9/19/17.
+//  Copyright © 2017 Slava Zubrin. All rights reserved.
+//
+
+import UIKit
+
+class CustomRefreshControl: UIControl {
+    
+    static let minOffsetToTriggerRefresh: CGFloat = 80;
+    
+    var cycleLength: Float = 30 {
+        didSet { setNeedsDisplay() }
+    }
+    var currentValue: Float = 0 {
+        didSet { setNeedsDisplay() }
+    }
+    
+    // MARK: Lifecycle
+    
+    override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        
+        let halfSize: Float = 10
+        let alpha = 2 * Float.pi * currentValue / cycleLength
+        
+        let start = CGPoint(x: rect.midX + CGFloat(cos(alpha) * halfSize),
+                            y: rect.midY + CGFloat(sin(alpha) * halfSize))
+        context?.move(to: start)
+        let finish = CGPoint(x: rect.midX + CGFloat(cos(alpha + Float.pi) * halfSize),
+                            y: rect.midY + CGFloat(sin(alpha + Float.pi) * halfSize))
+
+        context?.addLine(to: finish)
+        context?.setLineWidth(2)
+        context?.setStrokeColor(UIColor.red.cgColor)
+        context?.strokePath()
+    }
+    
+    // MARK: Public
+    
+    func containingScrollViewDidScroll(_ scrollView: UIScrollView) {
+        currentValue = Float(scrollView.contentOffset.y)
+        print(currentValue)
+    }
+    
+    func containingScrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView.contentOffset.y <= -CustomRefreshControl.minOffsetToTriggerRefresh {
+            sendActions(for: .valueChanged)
+        }
+    }
+}
---
+++ CustomSolutions/pull2refresh/Pull2RefreshViewController.swift
@@ -0,0 +1,72 @@
+//
+//  Pull2RefreshViewController.swift
+//  CustomSolutions
+//
+//  Created by Slava Zubrin on 9/19/17.
+//  Copyright © 2017 Slava Zubrin. All rights reserved.
+//
+
+import UIKit
+
+class Pull2RefreshViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var refreshControl: CustomRefreshControl = CustomRefreshControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.alwaysBounceVertical = true
+        scrollView.contentSize = CGSize(width: scrollView.bounds.size.width,
+                                        height: 2.0 * scrollView.bounds.size.height)
+        
+        setupRefreshControl()
+    }
+    
+    // MARK: User actions
+    
+    func handlePullToRefresh(_ sender: CustomRefreshControl) {
+        print("pulled to refresh")
+    }
+    
+    // MARK: Private
+    
+    private func setupRefreshControl() {
+        refreshControl.backgroundColor = UIColor.yellow
+        
+        refreshControl.addTarget(self, action: #selector(handlePullToRefresh(_:)), for: .valueChanged)
+        
+        refreshControl.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(refreshControl)
+        
+        let bindings: [String: UIView] = ["refresh": refreshControl]
+        NSLayoutConstraint
+            .constraints(withVisualFormat: "H:|-0-[refresh(==100)]-0-|",
+                                       options: [],
+                                       metrics: nil,
+                                       views: bindings)
+            .forEach { constraint in
+                constraint.isActive = true
+            }
+        NSLayoutConstraint
+            .constraints(withVisualFormat: "V:|-(-100)-[refresh(==100)]",
+                         options: [],
+                         metrics: nil,
+                         views: bindings)
+            .forEach { constraint in
+                constraint.isActive = true
+            }
+    }
+}
+
+extension Pull2RefreshViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        refreshControl.containingScrollViewDidScroll(scrollView)
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        refreshControl.containingScrollViewDidEndDragging(scrollView, willDecelerate: decelerate)
+    }
+}
